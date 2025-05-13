603f1348152ab633fb01e7688c979d399e8fc900
jyotiranjanpradhan <jrpliku331@gmail.com>
2025-05-13 18:07:16 +0530

    addProduct and view

---
+++ src/services/Product/apiProductForm.js
@@ -0,0 +1,24 @@
+import toast from "react-hot-toast";
+import apiGateWay from "../ApiGateWay/apiGateWay";
+
+export const postProductForm = async (data) => {
+  try {
+    const res = await apiGateWay.post(`/api/project_new_handler/`, data);
+    if (res.status == 201) {
+      return res.status;
+    }
+  } catch (error) {
+    console.log(error);
+    toast.error("Error on Post of Product Data");
+  }
+};
+
+export const getProductForm = async (url) => {
+  try {
+    const res = await apiGateWay.get(`${url}`, );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    toast.error("Error on Post of Product Data");
+  }
+};
